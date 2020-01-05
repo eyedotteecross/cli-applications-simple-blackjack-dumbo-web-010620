187c3d8203be604957dafb0f3a1b53a754077a79
Anthony Holder <anthonyh202x@gmail.com>
2020-01-05 19:44:05 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,9 +1,7 @@
-##################
-# shared methods #
-##################
+
 
 def welcome
-  # code #welcome here
+  p "Welcome!"
 end
 
 def deal_card
@@ -13,11 +11,6 @@ end
 def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
 end
-
-
-##########
-# rounds #
-##########
 
 def welcome
   puts "Welcome to the Blackjack Table"
@@ -61,19 +54,19 @@ def hit?(card_total)
     card_total += deal_card
   end
   card_total
-  
-  ## Alternative solution - this solution calls hit? recursively if given an invalid command
-  # if user_input == "h"
-  #   card_total += deal_card
-  # elsif user_input == "s"
-  #   card_total
-  # else
-  #   invalid_command
-  #   hit?(card_total)
-  # end
-  
+
 end
 
 def invalid_command
   puts "Please enter a valid command"
+end
+
+def runner
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
